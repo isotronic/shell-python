4bbba924e4f30bff2fbda173aaa50a985bb18a2f
Joseph Bouqdib <joseph@bouqdib.com>
2024-07-05 07:20:15 +0100

    fix: stop script from exiting after first command

--- app/main.py
+++ app/main.py
@@ -4,14 +4,14 @@ def check_command(user_input):
     return f"{user_input}: command not found"
 
 def main():
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
+    while True:
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
 
-    user_input = input()
-    output = check_command(user_input)
-    sys.stdout.write(output)
+        user_input = input()
+        output = check_command(user_input)
+        sys.stdout.write(f"{output}\n")
     
-
 
 if __name__ == "__main__":
     main()
