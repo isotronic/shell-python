4fe5affc9e73b4c760720abcaa08f4cdbcb99367
Joseph Bouqdib <joseph@bouqdib.com>
2024-07-05 08:25:31 +0100

    the type builtin: builtins

--- app/main.py
+++ app/main.py
@@ -1,13 +1,20 @@
 import sys
 
+available_command = ["type", "echo", "exit"]
+
 def check_command(user_command):
     command = user_command.split(" ", 1)[0]
     argument = user_command.split(" ", 1)[1] if " " in user_command else None
     match command:
-        case "exit":
-            sys.exit()
+        case "type":
+            if argument in available_command:
+                return f"{argument} is a shell builtin"
+            else:
+                return f"{argument}: not found"
         case "echo":
             return argument
+        case "exit":
+            sys.exit()
         case _:
             return f"{user_command}: command not found"
     
