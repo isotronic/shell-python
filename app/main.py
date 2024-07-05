f13aa1822f06a681147592793ca4835f058f4e2d
Joseph Bouqdib <joseph@bouqdib.com>
2024-07-05 14:49:24 +0100

    the cd builtin: absolute paths

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import sys
 import shutil
 import subprocess
 
-BUILTINS = ["type", "echo", "pwd", "exit"]
+BUILTINS = ["type", "echo", "cd", "pwd", "exit"]
 
 def check_command(user_command):
     command, *args = user_command.split(" ")
@@ -18,6 +18,20 @@ def check_command(user_command):
                 return f"{args[0]} is {path}"
         case "echo":
             return " ".join(args)
+        case "cd":
+            if len(args) == 0:
+                return "cd: missing argument"
+            try:
+                os.chdir(args[0])
+            except FileNotFoundError:
+                return f"cd: {args[0]}: No such file or directory"
+            except NotADirectoryError:
+                return f"cd: {args[0]}: Not a directory"
+            except PermissionError:
+                return f"cd: {args[0]}: Permission denied"
+            except Exception as e:
+                return f"cd: {args[0]}: {e}"
+            return None
         case "pwd":
             return os.getcwd()
         case "exit":
@@ -25,7 +39,7 @@ def check_command(user_command):
         case _:
             if shutil.which(command):
                 subprocess.run([command, *args])
-                return
+                return None
             return f"{user_command}: command not found"
     
 
